8043aadbbe7cd14ba8e43580d94bf4577667a546
Khoa Vu <khoa.vu@kyanon.digital>
2022-05-20 10:34:05 +0700

    [custom] layout

--- lib/screens/menuScreen.dart
+++ lib/screens/menuScreen.dart
@@ -1,6 +1,10 @@
-import "package:flutter/material.dart";
-import '../shared/bottomNavigation.dart';
-import "../styles/theme.dart" as Style;
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/loader.dart';
+import 'package:flutter_application_1/shared/getSources.dart';
+
+import '../../models/source.dart';
+import '../../models/sourceResponse.dart';
+import '../../screens/sourceDetail.dart';
 
 class MenuScreen extends StatefulWidget {
   @override
@@ -9,19 +13,116 @@ class MenuScreen extends StatefulWidget {
 
 class _MenuScreenState extends State<MenuScreen> {
   @override
+  void initState() {
+    super.initState();
+    getSources..getSources();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50.0),
-        child: AppBar(
-          backgroundColor: Style.Colors.mainColor,
-          title: Text(
-            "News App",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ),
+    return StreamBuilder<SourceResponse>(
+      stream: getSources.subject.stream,
+      builder: (context, AsyncSnapshot<SourceResponse> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data.error != null && snapshot.data.error.length > 0) {
+            return Container();
+          }
+          return _buildSourcesWidget(snapshot.data);
+        } else if (snapshot.hasError) {
+          return Container();
+        } else {
+          return buildLoadingWidget();
+        }
+      },
     );
+  }
+
+  Widget _buildSourcesWidget(SourceResponse data) {
+    List<SourceModel> sources = data.sources;
+    if (sources.length == 0) {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  "No More Sources",
+                  style: TextStyle(color: Colors.black45),
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    } else
+      return GridView.builder(
+        itemCount: sources.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, childAspectRatio: 0.86),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 5.0, right: 5.0, top: 10.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SourceDetail(
+                              source: sources[index],
+                            )));
+              },
+              child: Container(
+                width: 100.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey[100],
+                      blurRadius: 5.0,
+                      spreadRadius: 1.0,
+                      offset: Offset(
+                        1.0,
+                        1.0,
+                      ),
+                    )
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Hero(
+                      tag: sources[index].id,
+                      child: Container(
+                        height: 60.0,
+                        width: 60.0,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    "assets/logos/${sources[index].id}.png"),
+                                fit: BoxFit.cover)),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(
+                          left: 10.0, right: 10.0, top: 15.0, bottom: 15.0),
+                      child: Text(
+                        sources[index].name,
+                        textAlign: TextAlign.center,
+                        maxLines: 2,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 12.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
   }
 }
