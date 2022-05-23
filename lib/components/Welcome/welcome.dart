f4193a5895bbd77bb6012179196e16b7aec15350
Tan Loc <tanlocit20062000@gmail.com>
2022-05-23 12:00:20 +0700

    [add] screen welcome

---
+++ lib/components/Welcome/welcome.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../../constants.dart';
+import '../../models/allinonboardscreen.dart';
+import 'components/body.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
